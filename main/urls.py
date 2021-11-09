227d851a6214e9a8d8b7919e36a1079a72c8baf0
jmisrikhanov <jmisrikhanov@gmail.com>
2021-11-09 09:16:02 +0100

    completed project

--- main/urls.py
+++ main/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apiTodo.urls')),
+    path('', include("apiTodo.urls")),
+    
 ]
